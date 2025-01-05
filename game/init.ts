191760ee89a28707cca64a729aa30defb63f8886
yjgaia <yjdev88@gmail.com>
2025-01-05 18:24:33 +0900

    Add LoginPopup component and initialize Supabase connection

--- game/init.ts
+++ game/init.ts
@@ -1,7 +1,9 @@
 import { SPAInitializer } from "@common-module/app";
+import { SupabaseConnector } from "@common-module/supabase";
 import AppConfig, { IAppConfig } from "./AppConfig.js";
 
 export default async function init(config: IAppConfig) {
   AppConfig.init(config);
   SPAInitializer.init();
+  SupabaseConnector.init(AppConfig.supabaseUrl, AppConfig.supabaseKey);
 }
